964893f8229d71f393cbb116e4d033c60a212579
Esmee29 <Esmee.Fulcher@netmatters-scs.com>
2024-02-20 12:41:33 +0000

    Email boxes

--- js/main.js
+++ js/main.js
@@ -1,33 +1,33 @@
-document.getElementById('generate-image').addEventListener('click', function() {
+// Function to fetch and display a random image
+function displayRandomImage() {
   fetch('https://picsum.photos/600/300')
     .then(response => response.blob())
     .then(images => {
       // Create URL for the image
-      let outside = URL.createObjectURL(images);
+      let imageLink = URL.createObjectURL(images);
 
       // Get the image container
       let container = document.getElementById('image-container');
-
-      // Change the class of the container
-      container.className = 'image';
 
       // Remove existing image if any
       let oldImage = document.getElementById('display-image');
       if (oldImage) {
         oldImage.remove();
       }
-
       // Create new image element
       let img = document.createElement('img');
       img.id = 'display-image';
-      img.src = outside;
-      img.alt = 'Random Image';
-      img.className = 'responsive-image'; // Add this line
-
-      // Append the image to the container
+      img.src = imageLink;
+      img.alt = ' ';
+      img.className = 'responsive-image'; 
       container.appendChild(img);
     })
-    .catch(e => {
-      console.log(e);
+    .catch(error => {
+      console.log(error);
     });
-});
+}
+// Display a random image when the page loads
+displayRandomImage();
+
+// Change the image when the button is clicked
+document.getElementById('generate-image').addEventListener('click', displayRandomImage);
